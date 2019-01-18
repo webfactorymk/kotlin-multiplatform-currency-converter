5c65760124bf41bca82dda48e384eb14f0380427
Dimitar Zabaznoski <dimitar.zabaznoski@webfactory.mk>
2019-01-18 02:17:36 +0100

    Remove "manual" binding

--- android/app/src/main/java/mk/webfactory/dz/currencyconverter/MainActivity.kt
+++ android/app/src/main/java/mk/webfactory/dz/currencyconverter/MainActivity.kt
@@ -2,17 +2,9 @@ package mk.webfactory.dz.currencyconverter
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.Spinner
-import mk.webfactory.dz.currencyconverter.util.bind
+import mk.webfactory.dz.currencyconverter.R
 
 class MainActivity : AppCompatActivity() {
-
-    private val amountEditText: EditText by bind(R.id.edit_from_amount)
-    private val fromCurrencySpinner: Spinner by bind(R.id.spinner_from_currency)
-    private val toCurrencySpinner: Spinner by bind(R.id.spinner_to_currency)
-    private val switchCurrenciesButton: ImageButton by bind(R.id.img_btn_switch)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
